85be725d32b3ca65ed9d38473f7e5fd7601164dc
Emre <79359067+erenemrec23@users.noreply.github.com>
2026-07-02 13:35:28 +0300

    app role controller ve serviceleri eklendi

---
+++ TemplateProject.Application/Interfaces/ISecuredRequest.cs
@@ -0,0 +1,16 @@
+﻿using QrAssignment.Domain.Shared; // PagePermissions enum'ının olduğu yer
+
+namespace QrAssignment.Application.Interfaces
+{
+    public interface ISecuredRequest
+    {
+        // Yetkinin kontrol edileceği sayfanın/modülün adı (Örn: "Page_Tenants")
+        string PageName { get; }
+
+        // Bu işlem için gereken bitwise yetki değeri (Örn: PagePermissions.ExportExcel)
+        PagePermissions RequiredPermission { get; }
+    }
+    public interface INotSecuredRequest
+    { 
+    }
+}
