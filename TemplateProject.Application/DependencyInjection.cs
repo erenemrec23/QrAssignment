7a110e4892f8dac0982be24599d01d706c7e003b
Emre <79359067+erenemrec23@users.noreply.github.com>
2026-07-23 15:44:20 +0300

    AddApplicationServices sıralama değiştirildi

--- TemplateProject.Application/DependencyInjection.cs
+++ TemplateProject.Application/DependencyInjection.cs
@@ -9,19 +9,22 @@ namespace QrAssignment.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            // typeof(DependencyInjection) yerine .Assembly ekliyoruz
             services.AddAutoMapper(cfg => { }, typeof(DependencyInjection));
 
-            services.AddMediatR(cfg => {
+            services.AddMediatR(cfg =>
+            {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-                cfg.AddOpenBehavior(typeof(UnitOfWorkBehavior<,>));
 
-                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
+                // Sıralama önemli: liste yukarıdan aşağıya, en dıştan en içe doğru sarılır.
+                cfg.AddOpenBehavior(typeof(ExceptionHandlingBehavior<,>)); // 1. En dış: hiçbir exception dışarı sızmasın
+                cfg.AddOpenBehavior(typeof(AuthorizationBehavior<,>));     // 2. Yetki kontrolü, handler'a hiç girmesin
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));        // 3. FluentValidation kontrolü
+                cfg.AddOpenBehavior(typeof(UnitOfWorkBehavior<,>));        // 4. En iç: handler'a bitişik, SaveChanges burada
             });
 
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             services.AddValidatorsFromAssembly(typeof(SharedResource).Assembly);
             services.AddTransient(typeof(IValidator<>), typeof(GetByIdQueryValidator<>));
+
             return services;
         }
     }
