9aefc04181524520b45ebb3ec12c9e2ab239cd80
Emre <79359067+erenemrec23@users.noreply.github.com>
2026-07-23 15:52:41 +0300

    bug fixed

--- TemplateProject.Application/DependencyInjection.cs
+++ TemplateProject.Application/DependencyInjection.cs
@@ -14,12 +14,10 @@ namespace QrAssignment.Application
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-
-                // Sıralama önemli: liste yukarıdan aşağıya, en dıştan en içe doğru sarılır.
-                cfg.AddOpenBehavior(typeof(ExceptionHandlingBehavior<,>)); // 1. En dış: hiçbir exception dışarı sızmasın
-                cfg.AddOpenBehavior(typeof(AuthorizationBehavior<,>));     // 2. Yetki kontrolü, handler'a hiç girmesin
-                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));        // 3. FluentValidation kontrolü
-                cfg.AddOpenBehavior(typeof(UnitOfWorkBehavior<,>));        // 4. En iç: handler'a bitişik, SaveChanges burada
+                 
+                cfg.AddOpenBehavior(typeof(AuthorizationBehavior<,>));     
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));        
+                cfg.AddOpenBehavior(typeof(UnitOfWorkBehavior<,>));        
             });
 
             services.AddValidatorsFromAssembly(typeof(SharedResource).Assembly);
--- TemplateProject.WebApi/Program.cs
+++ TemplateProject.WebApi/Program.cs
@@ -1,13 +1,9 @@
-using FluentValidation;
-using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Localization; 
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
 using Microsoft.IdentityModel.Tokens;
 using QrAssignment.Application;
-using QrAssignment.Application.Behaviors;
 using QrAssignment.Application.Interfaces;
-using QrAssignment.Domain.Shared.Serialization;
 using QrAssignment.Infrastructure;
 using QrAssignment.Infrastructure.Localization;
 using QrAssignment.Persistance;
