20ba0cd3e879d931582f90b179416d77c465eb50
Emre <79359067+erenemrec23@users.noreply.github.com>
2026-08-10 12:09:39 +0300

    app.MapGet("/health", () => Results.Ok(new { status = "Healthy", time = DateTime.UtcNow }))    .AllowAnonymous(); eklendi

--- TemplateProject.WebApi/Program.cs
+++ TemplateProject.WebApi/Program.cs
@@ -210,6 +210,8 @@ app.UseSerilogUi(options =>
 {
     options.WithRoutePrefix("logs");
 });
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy", time = DateTime.UtcNow }))
+   .AllowAnonymous();
 app.MapControllers()
    .RequireRateLimiting("IpBasedRateLimit")
    .RequireAuthorization();
